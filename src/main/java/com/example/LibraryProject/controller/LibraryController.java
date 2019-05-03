f31626f9fbe0ac0a64bd2c27839639a8e323149b
zarzyckamar <zarzyckamar@gmail.com>
2019-05-03 19:53:14 +0200

    create method

--- src/main/java/com/example/LibraryProject/controller/LibraryController.java
+++ src/main/java/com/example/LibraryProject/controller/LibraryController.java
@@ -21,43 +21,13 @@ public class LibraryController {
 
     @RequestMapping(value = "/byISBN", method = RequestMethod.GET)
     public BookByISBN getBookByISBN(@RequestParam(name = "isbn") String isbn) throws FileNotFoundException {
+        return libraryService.getBookByISBN(isbn, libraryService.creatOutputModel());
+    }
 
-        LibraryFromJson libraryFromJson = libraryService.getJsonToObject();
-        ArrayList<BookByISBN> listofBookByISBNS = new ArrayList<>();
+    @RequestMapping(value = "/byCategory", method = RequestMethod.GET)
+    public BookByISBN getBookByCategory(@RequestParam(name = "category") String category) throws FileNotFoundException {
 
-        for (int i = 0; i < libraryFromJson.getItems().size(); i++) {
-
-            BookByISBN bookByISBN = new BookByISBN();
-            for (int n = 0; n < libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().size(); n++) {
-                if (libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().get(n).getType().equals("ISBN_13")) {
-                    bookByISBN.setIsbn(libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().get(n).getIdentifier());
-                } else {
-                    bookByISBN.setIsbn(libraryFromJson.getItems().get(i).getId());
-                }
-            }
-            bookByISBN.setTitle(libraryFromJson.getItems().get(i).getVolumeInfo().getTitle());
-            bookByISBN.setSubtitle(libraryFromJson.getItems().get(i).getVolumeInfo().getSubtitle());
-            try {
-                bookByISBN.setPublishedDate(Integer.parseInt(libraryFromJson.getItems().get(i).getVolumeInfo().getPublishedDate()));
-            } catch (NumberFormatException e) {
-                continue;
-            }
-            bookByISBN.setDescription(libraryFromJson.getItems().get(i).getVolumeInfo().getDescription());
-            bookByISBN.setPageCount(libraryFromJson.getItems().get(i).getVolumeInfo().getPageCount());
-            bookByISBN.setThumbnailUrl(libraryFromJson.getItems().get(i).getVolumeInfo().getImageLinks().getThumbnail());
-            bookByISBN.setLanguage(libraryFromJson.getItems().get(i).getVolumeInfo().getLanguage());
-            bookByISBN.setPreviewLink(libraryFromJson.getItems().get(i).getVolumeInfo().getPreviewLink());
-            try {
-                bookByISBN.setAuthors(libraryFromJson.getItems().get(i).getVolumeInfo().getAuthors());
-            } catch (NullPointerException e) {
-                System.out.print("null");
-                continue;
-            }
-            bookByISBN.setCategories(libraryFromJson.getItems().get(i).getVolumeInfo().getCategories());
-            listofBookByISBNS.add(bookByISBN);
-
-        }
-        return libraryService.getBookByISBN(isbn, listofBookByISBNS);
+        return null;
     }
 
 }
--- src/main/java/com/example/LibraryProject/service/LibraryService.java
+++ src/main/java/com/example/LibraryProject/service/LibraryService.java
@@ -20,13 +20,57 @@ public class LibraryService {
         return library;
     }
 
-    public BookByISBN getBookByISBN(String isbn, ArrayList<BookByISBN> books){
-        for(int i=0; i<books.size(); i++)
-            if(isbn.equals(books.get(i).getIsbn())) {
+    public BookByISBN getBookByISBN(String isbn, ArrayList<BookByISBN> books) {
+        for (int i = 0; i < books.size(); i++)
+            if (isbn.equals(books.get(i).getIsbn())) {
                 return books.get(i);
             }
         return null;
-}
+    }
 
+    public ArrayList<BookByISBN> creatOutputModel() {
+
+        LibraryFromJson libraryFromJson = null;
+        try {
+            libraryFromJson= getJsonToObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<BookByISBN> listofBookByISBNS = new ArrayList<>();
+
+        for (int i = 0; i < libraryFromJson.getItems().size(); i++) {
+
+            BookByISBN bookByISBN = new BookByISBN();
+            for (int n = 0; n < libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().size(); n++) {
+                if (libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().get(n).getType().equals("ISBN_13")) {
+                    bookByISBN.setIsbn(libraryFromJson.getItems().get(i).getVolumeInfo().getIndustryIdentifiers().get(n).getIdentifier());
+                } else {
+                    bookByISBN.setIsbn(libraryFromJson.getItems().get(i).getId());
+                }
+            }
+            bookByISBN.setTitle(libraryFromJson.getItems().get(i).getVolumeInfo().getTitle());
+            bookByISBN.setSubtitle(libraryFromJson.getItems().get(i).getVolumeInfo().getSubtitle());
+            try {
+                bookByISBN.setPublishedDate(Integer.parseInt(libraryFromJson.getItems().get(i).getVolumeInfo().getPublishedDate()));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+            bookByISBN.setDescription(libraryFromJson.getItems().get(i).getVolumeInfo().getDescription());
+            bookByISBN.setPageCount(libraryFromJson.getItems().get(i).getVolumeInfo().getPageCount());
+            bookByISBN.setThumbnailUrl(libraryFromJson.getItems().get(i).getVolumeInfo().getImageLinks().getThumbnail());
+            bookByISBN.setLanguage(libraryFromJson.getItems().get(i).getVolumeInfo().getLanguage());
+            bookByISBN.setPreviewLink(libraryFromJson.getItems().get(i).getVolumeInfo().getPreviewLink());
+            try {
+                bookByISBN.setAuthors(libraryFromJson.getItems().get(i).getVolumeInfo().getAuthors());
+            } catch (NullPointerException e) {
+                System.out.print("null");
+                continue;
+            }
+            bookByISBN.setCategories(libraryFromJson.getItems().get(i).getVolumeInfo().getCategories());
+            listofBookByISBNS.add(bookByISBN);
+
+        }
+        return listofBookByISBNS;
+    }
 
 }
