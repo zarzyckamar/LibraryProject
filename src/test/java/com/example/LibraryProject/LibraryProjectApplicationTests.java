d69c32864d6551c43c9bbed5b0b99a3d5492bcbe
zarzyckamar <zarzyckamar@gmail.com>
2019-05-05 14:43:01 +0200

    added test

--- src/test/java/com/example/LibraryProject/LibraryProjectApplicationTests.java
+++ src/test/java/com/example/LibraryProject/LibraryProjectApplicationTests.java
@@ -4,12 +4,17 @@ import com.example.LibraryProject.model.LibraryFromJson;
 import com.example.LibraryProject.modelEndpoint.BookByISBN;
 import com.example.LibraryProject.service.LibraryService;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.AssertTrue;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -31,20 +36,37 @@ public class LibraryProjectApplicationTests {
 	@Test
 	public void IsNotNullBookWithISBN() {
 		LibraryService libraryService = new LibraryService();
-		assertNotNull(libraryService.getBookByISBN("N1IiAQAAIAAJ",libraryService.creatOutputModel()));
+		assertNotNull(libraryService.getBookByISBN("N1IiAQAAIAAJ", libraryService.creatOutputModel()));
 	}
 
 	@Test
 	public void IsCorrectBookWithISBNnr1() {
 		LibraryService libraryService = new LibraryService();
-		BookByISBN bookByISBN = libraryService.getBookByISBN("N1IiAQAAIAAJ",libraryService.creatOutputModel());
+		BookByISBN bookByISBN = libraryService.getBookByISBN("N1IiAQAAIAAJ", libraryService.creatOutputModel());
 		assertEquals(bookByISBN.getIsbn(), "N1IiAQAAIAAJ");
 	}
 
 	@Test
-	public void IsNotNullComputersCategory(){
+	public void IsNotNullComputersCategory() {
 		LibraryService libraryService = new LibraryService();
 		ArrayList<BookByISBN> bookByISBN = libraryService.getBookByCategory("Computers", libraryService.creatOutputModel());
 		assertNotNull(bookByISBN);
 	}
+
+	@Test
+	public void pingTest() {
+		given().when().get("/api/all").then().statusCode(200);
+	}
+
+	@Test
+	public void verifyTitleOfBook() {
+		given()
+				.when()
+				.get("/api/byISBN?isbn=gJEC2q7DzpQC")
+				.then()
+				.statusCode(200)
+				.body("title", equalTo("The History of Java"));
+	}
+
+
 }
